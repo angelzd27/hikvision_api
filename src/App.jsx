84c35e56b5cd86027ba1bdae668875e7af4cddf5
decidesuici <eduardx62@outlook.com>
2024-12-11 17:18:27 -0600

    test

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
         <h1 className='text-5xl font-bold'>Hikvision test</h1>
         <div>
           <div>This is a test</div>
+          test2
         </div>
       </div>
     </>
